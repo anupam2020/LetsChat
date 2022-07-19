511ebcf50f7921303c425203fe6a0d32a51bbc3f
Anupam Basak <anupam00basak@gmail.com>
2022-07-19 23:05:33 +0530

    Token and Data classes added!

---
+++ app/src/main/java/com/sbdev/letschat/Token.java
@@ -0,0 +1,21 @@
+package com.sbdev.letschat;
+
+public class Token {
+
+    String token_id;
+
+    public Token(String token_id) {
+        this.token_id = token_id;
+    }
+
+    public Token() {
+    }
+
+    public String getToken_id() {
+        return token_id;
+    }
+
+    public void setToken_id(String token_id) {
+        this.token_id = token_id;
+    }
+}
